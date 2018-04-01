3b6253c8acd19947125bc730dc02145cac7ffaf3
Simon CORNAZ <scornaz@e2r5p18.42.fr>
2018-04-01 20:39:22 +0200

    OK vraiment meilleur doublon

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/31 21:30:19 by scornaz           #+#    #+#             */
-/*   Updated: 2018/04/01 20:22:56 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/04/01 20:39:06 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@
 void		multiple_connexions(t_array *array, char *line, char **split_line)
 {
 	char	*tmp;
+	t_array	*inverse_str;
 
-	tmp = 0;
-	if (array->mem && ft_strstr(array->mem, line))
+	inverse_str = array_new(1, 8);
+	array_add(inverse_str, split_line[1], ft_strlen(split_line[1]));
+	array_add(inverse_str, "-", 1);
+	array_add(inverse_str, split_line[0], ft_strlen(split_line[0]));
+	tmp = inverse_str->mem;
+	tmp[inverse_str->cursor] = 0;
+	if (array->mem && (ft_strstr(array->mem, line)
+					   || ft_strstr(array->mem, tmp)))
 		return ;
 	array_add(array, line, ft_strlen(line));
 	array_add(array, " ", 1);
-	array_add(array, split_line[1], ft_strlen(split_line[1]));
-	array_add(array, "-", 1);
-	array_add(array, split_line[0], ft_strlen(split_line[0]));
+	array_add(array, tmp, ft_strlen(tmp));
 	array_add(array, " ", 1);
+	array_free(inverse_str);
 }
 
 char		*get_txt(void)
